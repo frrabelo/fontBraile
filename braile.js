902df627dd5622f4442e7399311468426acc29dc
Francisco Rabelo <37917771+frrabelo@users.noreply.github.com>
2024-05-11 20:09:31 -0300

    Update braile.js
    alterando nome do arquivo

--- braile.js
+++ braile.js
@@ -1,35 +1,6 @@
-//Fonte braile para ser enbedad ao seu html
-
-// FonteBraile.js
-
-// Função para incorporar a fonte braile.ttf ao CSS do documento
-//function embedBrailleFont() {
-    // Cria um novo estilo
-//    var style = document.createElement('style');
-//    style.appendChild(document.createTextNode(''));
-
-    // Adiciona a regra CSS para aplicar a fonte braile.ttf a todos os elementos do HTML
-//    style.innerHTML = `
-//        @font-face {
-            
-//            src: url('braile.ttf');
-//            font-weight: normal;
-//            font-style: normal;
- //       }
-//    `;
-
-    // Adiciona o estilo ao cabeçalho do documento (head)
-//    document.head.appendChild(style);
-//}
-
-// Chama a função para incorporar a fonte braile.ttf ao CSS assim que o script é carregado
-//embedBrailleFont();
-
-
-
 (function() {
   // Carrega a fonte Braille
-  var fontFace = new FontFace('Braille', 'url(Braile_font.ttf)');
+  var fontFace = new FontFace('Braille', 'url(Braile.ttf)');
   fontFace.load().then(function(font) {
     document.fonts.add(font);
 
